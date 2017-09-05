22779b96537e3d79f22c05357443a2e08311adda
Unknown <cow86868686@gmail.com>
2017-09-05 11:55:08 +0800

    123

---
+++ 004.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var fruits:Array<String> = ["Apple","Orange","Bananas","Grapes","Guava"]
+
+var sum = 0
+
+for fruits in fruits {
+    print(fruits.count)
+}
+
